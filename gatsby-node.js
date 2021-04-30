7c48702553df168f8f9c8fde6703282de2f5779a
Dejan85 <xypnotica@gmail.com>
2021-04-30 17:05:49 +0200

    created dynamic query

---
+++ gatsby-node.js
@@ -0,0 +1,16 @@
+exports.createPages = async ({ actions, graphql }) => {
+  const { data } = await graphql(`
+    query {
+      allMdx(sort: { fields: frontmatter___date, order: DESC }) {
+        edges {
+          node {
+            id
+            frontmatter {
+              slug
+            }
+          }
+        }
+      }
+    }
+  `)
+}
