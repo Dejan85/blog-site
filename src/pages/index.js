8729772a5c6c82d402bf54158a558d29eb021b37
Dejan85 <xypnotica@gmail.com>
2021-04-30 14:21:33 +0200

    reafactored project setup

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,7 +1,5 @@
-import React from "react";
+import React from "react"
 
-const index = () => {
-  return <div>index page</div>;
-};
-
-export default index;
+export default function Home() {
+  return <div>Hello world!</div>
+}
